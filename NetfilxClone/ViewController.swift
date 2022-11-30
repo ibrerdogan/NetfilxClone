519d6c3b547f87e307d8ad1da277beea6078424f
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2022-11-30 22:41:27 +0300

    Initial Commit

---
+++ NetfilxClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetfilxClone
+//
+//  Created by İbrahim Erdogan on 30.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
